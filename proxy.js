461c95ac969af6825e31b351086647f591b281db
Collin Kokotas <ckokotas@stonecobra.com>
2016-08-31 16:37:30 -0700

    Stabalize.

--- proxy.js
+++ proxy.js
@@ -1,12 +1,11 @@
 "use strict";
 
 const SimpleServer = require('./lib/SimpleServer'),
-	cookieParser = require('cookie-parser'),
-	httpRequest = require('request'),
-	bodyParser = require('body-parser'),
-	Recorder = require('./lib/Recorder');
+	Recorder = require('./lib/Recorder'),
+	proxyAll = require('./lib/proxyAll');
 
 const CONFIG = require('./config.json'),
+	DEV_CONFIG = require('./dev/mock-server/config.json'),
 	PACKAGE = require('./package.json');
 
 // CLI argument parsing ///////////////////////////////////////////////////////////////////////////////////////////////
@@ -18,57 +17,32 @@ const ARGS = require('commander')
 
 // server definition //////////////////////////////////////////////////////////////////////////////////////////////////
 
-var targetUrl, startMarker, endMarker,
-	middleware = [
-		cookieParser(),
-		bodyParser.json()
-	];
-
-if(ARGS.dev){
-	const DEV_CONFIG = require('./dev/mock-server/config.json');
-
-	targetUrl = `http://localhost:${DEV_CONFIG.PORT}`;
-	startMarker = DEV_CONFIG.START_MARKER;
-	endMarker = DEV_CONFIG.END_MARKER;
-}else{
-	targetUrl = CONFIG.TARGET_URL;
-	startMarker = CONFIG.START_MARKER;
-	endMarker = CONFIG.END_MARKER;
-}
-
-/**
- * Setup routes
- */
-const routes = {
-	'ALL--*': (req, res) => {
-		const url = targetUrl + req.originalUrl,
-			method = req.method.toLowerCase();
-
-		if(!res.headersSent){
-			console.log(`Proxying request to: ${url}`);
-			httpRequest[method](url).on('error', console.error).pipe(res);
-		}else{
-			res.end();
-		}
-	}
-};
-
 /**
  * Create recorder
  */
 const recorder = new Recorder({
-	start: startMarker,
-	end: endMarker,
-	callback: instance => {
-		instance.replay = true;
-		console.log(instance.stringify(4));
+	start: (ARGS.dev ? DEV_CONFIG : CONFIG)['START_MARKER'],
+	end: (ARGS.dev ? DEV_CONFIG : CONFIG)['END_MARKER'],
+	callback: () => {
+		recorder.replay = true;
+		console.log(recorder.stringify(4));
 	}
 });
 
+/**
+ * Setup Proxy
+ */
 new SimpleServer({
 	port: CONFIG.PORT,
-	middleware: middleware.concat(recorder.middleware),
-	routes: routes
+	routes: {
+		'GET--/recording-proxy/dashboard'(req, res){
+			res.end('--TODO--');
+		}
+	},
+	middleware: [
+		recorder.middleware,
+		proxyAll(ARGS.dev ? DEV_CONFIG : CONFIG)
+	],
 });
 
 // helper abstractions ////////////////////////////////////////////////////////////////////////////////////////////////
